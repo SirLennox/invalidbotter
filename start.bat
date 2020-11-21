b3aac3028eacbc1a402ad747c46b64dcc731dba8
SirLennox <52826781+SirLennox@users.noreply.github.com>
2020-11-21 13:24:41 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,2 @@
-echo Be sure you have npm installed.
-echo If you don't have npm installed please install it and restart your PC if needed
-if not exist "node-modules" npm install
+if not exist "node-modules" start.bat
 npm run run
