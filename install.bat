a3cf09143d5e5c95c7001b8b34a0c5c09da25253
SirLennox <52826781+SirLennox@users.noreply.github.com>
2020-11-22 19:53:47 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,3 @@
 echo Installing packages... Please wait.
-echo Be sure to have npm installed.
-npm install
-if not exist "node-modules" echo It looks like you do not have npm installed. Please check your installation.
+echo Be sure to have npm installed and you executed this with adminstrator.
+npm install invalidbotter -g
